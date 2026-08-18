bf0564f9cb2f9bd850db3cc07ed5e56fbec451a9
Yadav <Rohit.Yadav1@landisgyr.com>
2026-08-18 17:57:17 +0530

    Update App

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello: This is my first CI/CD project")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from my GCP DevOps project!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
